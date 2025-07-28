38e333e70da39388002f897850d95a3de14ed3fa
Adarsha Das <adarshadas47@gmail.com>
2025-07-28 13:35:17 +0530

    update cros

--- leaderboard-backend/index.js
+++ leaderboard-backend/index.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
@@ -8,8 +8,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose;
 mongoose
- mongoose.connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log(err));
 
